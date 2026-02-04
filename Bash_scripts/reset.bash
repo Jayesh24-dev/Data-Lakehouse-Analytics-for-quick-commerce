e079d0443e3a8c3c0e1969dbc1447f2d46996cf5
unknown <you@example.com>
2026-02-04 09:22:32 +0530

    commited

---
+++ Bash_scripts/reset.bash
@@ -0,0 +1,13 @@
+hdfs dfs -rm -r /user/jayesh/project/checkpoints/orders_bronze/*
+hdfs dfs -rm -r /user/jayesh/project/checkpoints/orders_silver/*
+
+hdfs dfs -rm -r /user/jayesh/project/orders/orders_bronze/*
+hdfs dfs -rm -r /user/jayesh/project/orders/orders_silver/*
+
+
+hdfs dfs -rm -r /user/jayesh/project/orders/rnn_data/*
+hdfs dfs -rm -r /user/jayesh/project/orders/orders_gold/5min_window/*
+
+
+
+
